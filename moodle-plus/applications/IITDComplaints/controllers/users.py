69b3bb714e42815c468b398b139d5a4615636df8
cruck12 <sahilbhatnagar012@gmail.com>
2016-03-28 19:39:36 +0530

    Added get user from id API

--- moodle-plus/applications/IITDComplaints/controllers/users.py
+++ moodle-plus/applications/IITDComplaints/controllers/users.py
@@ -6,3 +6,10 @@ def getAll():
     users=[]
     users=db((db.users.id>0)&(db.users.verified==1)).select(db.users.id,db.users.first_name,db.users.type)
     return dict(users=users)
+
+def getUser():
+    if len(request.args)<1:
+        raise HTTP(404)
+    user_id = str(request.args[0]).lower()
+    users=db((db.users.id==user_id)&(db.users.verified==1)).select(db.users.id,db.users.first_name,db.users.type)
+    return dict(users=users)
